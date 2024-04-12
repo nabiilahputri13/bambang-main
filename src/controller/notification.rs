7f605fc637b2a47eceb117f52908f2caf0ee8a62
nabiilahputri13 <putrisafa1394@gmail.com>
2024-04-13 01:15:57 +0700

    Implement unsubscribe function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -6,17 +6,17 @@ use crate::model::subscriber::Subscriber;
 use crate::service::notification::NotificationService;
 
 #[post("/subscribe/<product_type>", data = "<subscriber>")]
-    pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
-        return match NotificationService :: subscribe(product_type, subscriber.into_inner()) {
-            0k(f) => Ok(Created :: new("/").body (Json :: from(f) )),
-            Err(e) => Err(e)
-        };
-    }
+pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
+    return match NotificationService :: subscribe(product_type, subscriber.into_inner()) {
+        0k(f) => Ok(Created :: new("/").body (Json :: from(f) )),
+        Err(e) => Err(e)
+    };
+}
 
-    pub fn route_stage() -> AdHoc {
-        return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
-            rocket
-                .mount("/product", routes![product::create, product::list, product::read, product::delete])
-                .mount("/notification", routes![notification::subscribe])
-        });
-    }
+#[post("/unsubscribe/<product_type> ?< url>")]
+pub fn unsubscribe(product_type: &str, url: &str) -> Result<Json<Subscriber>> {
+    return match NotificationService :: unsubscribe(product_type, url) {
+        0k(f) => Ok(Json :: from(f)),
+        Err(e) => Err(e)
+    };
+}
