5d1ab020503c67bc65fa86235beaf70f3a433f85
nabiilahputri13 <putrisafa1394@gmail.com>
2024-04-12 23:08:47 +0700

    Implement list_all function in Subscriber repository.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,6 +1,6 @@
-use dashmap: : DashMap;
-use lazy_static :: lazy_static;
-use crate: :model: : subscriber :: Subscriber;
+use dashmap::DashMap;
+use lazy_static::lazy_static;
+use crate::model::subscriber::Subscriber;
 
 // Singleton of Database
 lazy_static! {
@@ -8,14 +8,23 @@ static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap: 
 
 pub struct SubscriberRepository;
 impl SubscriberRepository {
-};
+    pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
+        let subscriber_value = subscriber. clone();
+        if SUBSCRIBERS.get (product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        };
+    
+        SUBSCRIBERS.get(product_type).unwrap ()
+            .insert(subscriber_value.url.clone(), subscriber_value);
+        return subscriber;
+    }
 
-SUBSCRIBERS. get(product_type) . unwrap ()
-.insert(subscriber_value.url.clone(), subscriber_value);
-return subscriber;
-
-pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
-let subscriber_value = subscriber. clone();
-if SUBSCRIBERS.get (product_type).is_none() {
-SUBSCRIBERS.insert(String :: from(product_type), DashMap: : new());
+    pub fn list_all(product_type: &str) -> Vec<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+        SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        };
+        
+        return SUBSCRIBERS. get(product_type).unwrap ().iter()
+        .map(|f| f.value().clone()).collect();
+        }
 }
